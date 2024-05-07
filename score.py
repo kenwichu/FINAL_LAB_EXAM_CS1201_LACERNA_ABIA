ad0f13efdcf8f0621bfb2b0f9252e9fce1939021
Creene7 <23-03219@g.batstate-u.edu.ph>
2024-05-07 10:00:21 +0800

    Add files via upload

---
+++ score.py
@@ -0,0 +1,17 @@
+
+def main():
+    print("Welcome")
+    print(1. Register)
+    print(2. Login)
+    print(3. Exit)
+    while(True:)
+            try:
+                choice = input("Enter your choice: ")
+                if choice == '1'
+                    pass
+                elif choice == '2'
+                    pass
+                elif choice == '3'
+                    pass
+
+            
