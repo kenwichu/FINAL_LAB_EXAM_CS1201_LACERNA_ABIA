5d55e64fc24efc43df12cb8d4a5baa975d9f203c
kchu <162998252+kenwichu@users.noreply.github.com>
2024-05-07 09:17:41 +0800

    Add files via upload

---
+++ dice_game.py
@@ -0,0 +1,51 @@
+import os
+import random
+
+class DiceGame:
+    def __init__(self):
+        self.users = {}
+
+        def register(self):
+           print ("Register: ")
+           username= input("Enter username: ")
+           password= input("Enter password: ")
+
+           if len (username) == 4 and len (password) == 8:
+              self.users 
+              
+
+
+
+
+
+
+
+
+        def load_scores(self):
+         pass
+
+        def save_scores(self):
+          pass
+
+        def play_game(self):
+           pass
+
+        def show_top_scores(self):
+             
+             while True:
+                Turn = 0
+                player_score = 0
+                cpu_score = 0
+                while Turn < 3:
+                   player= random.randint(1, 6)
+                   cpu= random.randint(1, 6)
+
+                   print("Player rolled.")
+                   pl
+
+        def log_out(self):
+           pass
+
+        def menu(self):
+          print("1. Login")
+          pass
